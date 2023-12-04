6399831f9fecd239760561c93a8bfba9c50dd875
zhangpeng <zhangpeng@wedobest.com.cn>
2023-12-04 11:00:44 +0800

    add router

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import { createRouter, createWebHistory  } from "vue-router";
+/**
+ * 路由配置
+ * @description 所有路由都在这里集中管理
+ */
+const routes = [
+  /**
+   * 首页
+   */
+  {
+    path: "/",
+    name:"Home",
+    component: () => import('../views/home/index.vue')
+  },
+  {
+    path: "/hello",
+    name:"Hello",
+    component: () => import('../views/hello/index.vue')
+  },
+//   /**
+//    * 子路由示例
+//    */
+//   {
+//     path: '/foo',
+//     name: 'foo',
+//     component: () => import('@/components/TransferStation.vue'),
+//     meta: {
+//       title: 'Foo',
+//     },
+//     redirect: {
+//       name: 'bar',
+//     },
+//     children: [
+//       {
+//         path: 'bar',
+//         name: 'bar',
+//         component: () => import('@/views/foo/bar.vue'),
+//         meta: {
+//           title: 'Bar',
+//         },
+//       },
+//     ],
+//   },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/home/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+      <global-header :user=user></global-header>
+      <column-list :list=testData></column-list>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import 'bootstrap/dist/css/bootstrap.min.css'
+  import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+  import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
+  
+  const user: UserProps = {
+    isLogin: false
+  }
+  const testData: ColumnProps[] = [
+    {
+      id: 1,
+      title: 'test1的专栏',
+      description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+      avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+    },
+    {
+      id: 2,
+      title: 'test2的专栏',
+      description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+      avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+    }
+  ]
+  </script>
+  
+  <style>
+
+  </style>
+  
